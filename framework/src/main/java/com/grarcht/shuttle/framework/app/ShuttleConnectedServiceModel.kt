cdc8b9367c1c1f8e0885e69fa487f2ca2f59cf6e
Gregory Rasmussen <gregory@grarcht.com>
2024-05-18 22:33:33 -0700

    Added comments

--- framework/src/main/java/com/grarcht/shuttle/framework/app/ShuttleConnectedServiceModel.kt
+++ framework/src/main/java/com/grarcht/shuttle/framework/app/ShuttleConnectedServiceModel.kt
@@ -1,7 +1,13 @@
 package com.grarcht.shuttle.framework.app
 
 import android.os.Messenger
+import com.grarcht.shuttle.framework.content.service.ShuttleServiceConnection
 
+/**
+ * Holds the service references, emitted from the [ShuttleServiceConnection].
+ * @param localService used for transporting cargo with the local service
+ * @param ipcMessenger used for transporting cargo with the remote service via IPC
+ */
 data class ShuttleConnectedServiceModel<S : ShuttleService>(
     var localService: S? = null,
     var ipcMessenger: Messenger? = null
